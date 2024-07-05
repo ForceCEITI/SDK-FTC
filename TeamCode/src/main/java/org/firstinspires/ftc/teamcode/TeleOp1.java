22e94f4aa85826b6b69a49ae3df22f73d3be3d67
Alex <belii.alexandra06@gmail.com>
2024-07-05 17:47:19 +0300

    code for arm stillness'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.google.ar.core.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+
 
 @TeleOp(name="Start")
 public class TeleOp1 extends OpMode {
@@ -15,6 +19,15 @@ public class TeleOp1 extends OpMode {
 
     Servo leftServo;
     Servo rightServo;
+    PIDController controller;
+
+    final double armPPR = 1425.1; //câte poziții are într-o rotație
+    double armTarget = 0 ;
+    public static double kG = 0.2;
+
+    //    public static double kP = 0, kI = 0, kD = 0;
+//    v private final double ticks_in_degree = 700 / 180.0;
+
 
     @Override
     public void init() {
@@ -33,6 +46,9 @@ public class TeleOp1 extends OpMode {
 
         leftServo.setPosition(0.55);
         rightServo.setPosition(0.65);
+
+        controller = new PIDController(1, 1, 1);
+
 
         telemetry.addLine("Hello!");
         telemetry.update();
@@ -66,11 +82,20 @@ public class TeleOp1 extends OpMode {
             rightServo.setPosition(0.75);
         }
 
+        double currentAngle = armMotor.getCurrentPosition() * 2 * Math.PI / armPPR;
+        double armFeedForword = armMotor.getCurrentPosition() * kG * Math.cos(currentAngle);
+        armMotor.setPower(gamepad1.right_trigger + armFeedForword);
 
-        armMotor.setPower(gamepad1.right_trigger);
-        armMotor.setPower(-gamepad1.left_trigger);
 
-        telemetry.addData("Time", getRuntime());
+
+//        controller.setPID(p,i, d);
+//        int armPos = armMotor.getCurrentPosition();
+//        double pid = controller.calculate(armPos, target);
+//        double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
+//        double power = pid + ff;
+
+
+        telemetry.addData("Angle", Math.toDegrees(currentAngle));
         telemetry.update();
     }
 }
